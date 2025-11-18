4e915d0121952164c78729e949be644840d81044
Chandler Barnes <chandler.barnes96@gmail.com>
2025-11-18 11:54:01 -0600

    feat: hide "Find Near Me" text on small screens

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -160,7 +160,7 @@ export const Filters: React.FC = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-                <span>Find Near Me</span>
+                <span className="lmw-hidden lg:lmw-inline">Find Near Me</span>
               </>
             )}
           </button>
